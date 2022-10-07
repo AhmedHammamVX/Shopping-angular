bd6cc2e5a6c3a3846a0307035208d60719e0da83
Kh-Anter <105430867+Kh-Anter@users.noreply.github.com>
2022-10-08 01:39:29 +0200

    edit on nodejs server

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,16 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { IUser } from './IUser';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
-import { IPost } from './IPost';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
- getUrl:string="https://dummyjson.com/users";
- postUrl:string="https://dummyjson.com/posts/add";
- commentsUrl:string="https://dummyjson.com/comments";
  registerUrl:string="http://localhost:3001/signUp";
  loginUrl:string="http://localhost:3001/login";
  accountsUrl:string="http://localhost:3001/accounts";
@@ -18,23 +14,6 @@ export class UsersService {
  getProductsUrl:string="http://localhost:3001/getProducts";
   constructor(private http:HttpClient) { }
   
-  getUsers():Observable<IUser[]> {
-    return this.http.get<IUser[]>(this.getUrl).pipe(catchError((err)=>{
-      return throwError(()=>(err.message || "Internal Server error"))
-    }))
-  }
-  
-  addPost(posts:IPost){
-   return this.http.post(this.postUrl,posts).pipe(catchError((error)=>{
-      return throwError(()=>error.message || "Internal server error")
-     }));
-  }
-
-  getComments():Observable<any> {
-    return this.http.get<any>(this.commentsUrl).pipe(catchError((err)=>{
-      return throwError(()=>(err.message || "Internal Server error"))
-    }))
-  }
 
   login(loginData:any):Observable<any>{
     return this.http.post(this.loginUrl,loginData).pipe(catchError((err)=>{
